696754f142a0b04c7eb74a1e4a586b19d0789a18
ekosampoerna <ekosampoerna7@gmail.com>
2018-12-18 02:43:14 +0700

    Add files via upload

---
+++ template/module/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> Bebas 0.1
+        </div>
+        <strong>Copyright &copy; 1400018010 <a href="#">PKPL</a>.</strong> All rights reserved.
+      </footer>
